979816d13838804ea4fcb2b5ba2286930221d829
Mathew Vieira <mvieirabezerra@gmail.com>
2024-08-16 01:33:20 -0300

    Ajustes Menores

--- public/index.php
+++ public/index.php
@@ -4,9 +4,9 @@ include '../config/routes.php';
 
 function view(string $name, mixed $data = []): void
 {
-  // include: ele tenta, se der erro, ele continua a aplicacao
-  // require: ele tenta, se der erro, ele para a aplicacao
-  // *_once: ele so inclui o arquivo uma vez
+    // include: ele tenta, se der erro, ele continua a aplicacao
+    // require: ele tenta, se der erro, ele para a aplicacao
+    // *_once: ele so inclui o arquivo uma vez
 
     include "../src/views/_template/head.php";
     include "../src/views/{$name}.php";
